0e94c9ddaf8a7e40622e2f0cda81b98a909fbe7b
Zack Shone <zackariahshone@gmail.com>
2020-07-29 21:59:19 -0700

    mmmm

--- Jscript/smtpEmail.js
+++ Jscript/smtpEmail.js
@@ -4,21 +4,6 @@ $(document).ready(function () {
 // const emailContent = $('#content').val();
 
 
-    $('#send').click(function(){
-        const userEmail = $('#email').val();
-        const emailContent = $('#content').val();
-    console.log('Email: ' + userEmail);
-    console.log('emailContent: ' + emailContent);
-
-        Email.send({
-                SecureToken : "d3ed9c5d-7d09-4951-80ed-7b25cdd4fb32",
-                To : "lareaufilms@gmail.com",
-                From : userEmail,
-                Subject : "Intrested in working together",
-                Body : emailContent
-        }).then(
-                message => alert(message)
-                );
-        });
+   
         
         });
